26a62c28126d4dc8fc93285ce2bfb8f61b3f7219
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-06-06 14:17:10 +0530

    withdraw fees instruction for protocol

--- programs/divvy/src/instructions.rs
+++ programs/divvy/src/instructions.rs
@@ -1,9 +1,13 @@
 pub mod initialize_split;
 pub mod add_member;
 pub mod deposit;
+//Protocol 
 pub mod initialize_protocol;
+pub mod withdraw_fees;
 
 pub use initialize_split::*;
 pub use add_member::*;
 pub use deposit::*;
+//Protocol
 pub use initialize_protocol::*;
+pub use withdraw_fees::*;
