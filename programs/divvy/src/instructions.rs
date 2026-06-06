c55f944e2ef8d8073f1978a4a80f025595ef5242
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-06-06 13:43:24 +0530

    initialize_protocol instruction

--- programs/divvy/src/instructions.rs
+++ programs/divvy/src/instructions.rs
@@ -1,3 +1,9 @@
 pub mod initialize_split;
+pub mod add_member;
+pub mod deposit;
+pub mod initialize_protocol;
 
 pub use initialize_split::*;
+pub use add_member::*;
+pub use deposit::*;
+pub use initialize_protocol::*;
